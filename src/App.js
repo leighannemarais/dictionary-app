bcf7cc474b7ae427490369d07a07f2b856fb081e
leighannemarais <leighannemarais@gmail.com>
2023-08-03 13:31:03 +0200

    Added dictionary API integration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src="https://www.shecodes.io/assets/branding/logo-shecodes--summer-42902242bc561967741362fbfcacbe7750d51da501d53b0e54d6498093bd0b75.png"
-          className="App-logo img-fluid"
-          alt="logo"
-        />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src="https://www.shecodes.io/assets/branding/logo-shecodes--summer-42902242bc561967741362fbfcacbe7750d51da501d53b0e54d6498093bd0b75.png"
+            className="App-logo img-fluid"
+            alt="logo"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          {" "}
+          <small>Coded by Leigh-Anne</small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse() {}
+
+  function search(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
