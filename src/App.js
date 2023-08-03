db2bbfe86f61760f4c692fbd42f911262a80698a
leighannemarais <leighannemarais@gmail.com>
2023-08-03 12:34:32 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://www.shecodes.io/assets/branding/logo-shecodes--summer-42902242bc561967741362fbfcacbe7750d51da501d53b0e54d6498093bd0b75.png"
+          className="App-logo img-fluid"
+          alt="logo"
+        />
+      </header>
+    </div>
+  );
+}
