b4224e1b449b85c774083928f4a760c6589fb453
leighannemarais <leighannemarais@gmail.com>
2023-08-04 12:21:06 +0200

    Fixed errors

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,7 +12,7 @@ export default function Dictionary(props) {
     setResults(response.data[0]);
   }
 
-  function search(keyword) {
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
