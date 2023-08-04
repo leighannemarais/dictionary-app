d27cf24e56da81f6ef909491fba387b27acf541a
leighannemarais <leighannemarais@gmail.com>
2023-08-04 12:11:39 +0200

    Fixed typing error

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,7 +5,7 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <div className="Synonyms">
-        <stong>Similar:</stong>
+        <strong>Similar:</strong>
         <ul>
           {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
